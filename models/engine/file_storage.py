d75c1a44ef8f6ad07000582881d51ae5aadd5c85
Augustine <augustine.alul@yahoo.com>
2023-08-10 21:24:08 +0100

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,7 @@ file storage module
 """
 import json
 
+
 class FileStorage:
     """
     serialises instances to a json file
@@ -15,7 +16,8 @@ class FileStorage:
         """
         :return:"__objects"
         """
-        
+        return __objects
+
     def new(self,obj):
         """
         sets in the dictionary "__objects"
@@ -25,10 +27,24 @@ class FileStorage:
         class_name = self.__class__.__name__
         key = "{}{}{}".format(class_name, '.', self.id)
         FileStorage.__objects[key] = obj
-        
+
     def save(self):
         """
         serialises the python dictionary __objects to
         the JSON file (path:__file_path)
         """
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
+            json.dump(File_Storage.__objects, file)
 
+    def reload(self):
+        """
+        deserialises the JSON file to
+        "--objects"
+        """
+        if FileStorage.__file_path != None:
+            try:
+                with open(FileStorage.__filepath, "w", encoding="utf-8") as file:
+                    FileStorage.__objects = json.load(file)
+            except Exception:
+                return
+        return
